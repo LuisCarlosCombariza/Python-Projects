abcdbaa994711f7e592bb8bda4dcf9c7fd736541
Luis Combariza <lcc340@mail.usask.ca>
2020-07-02 21:35:05 -0600

    Created initial game element classes that will soon become the space ships for the game

--- SpaceInvaders/GameImages/main.py
+++ SpaceInvaders/GameImages/main.py
@@ -33,6 +33,23 @@ YELLOW_LASER = pygame.image.load(os.path.join("pixel_laser_green.png"))
 BACK_GROUND = pygame.transform.scale(pygame.image.load(os.path.join("background-black.png")),(WIDTH,HEIGHT))
 
 
+# Game object classes
+# ------------------------------------------------------------------------------------------
+class Ship:
+    def __init__(self, location_x, location_y, health= 100):
+        self.location_x = location_x
+        self.location_y = location_y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self,window):
+        pygame.draw.rect(window,(255,0,0),(self.location_x,self.location_y, 50,50),0)
+
+
+
 # Even Main Loop
 # ------------------------------------------------------------------------------------------
 def main():
@@ -42,6 +59,7 @@ def main():
     lives = 5
     main_font = pygame.font.SysFont("comicsans",50)
     clock  = pygame.time.Clock()
+    ship = Ship(300,650)
 
     def redraw_window():
         # Adding the background image at coordinate (0,0)
@@ -50,8 +68,11 @@ def main():
         lives_label = main_font.render(f"Lives:{lives}",1,(250,0,0))
         level_label = main_font.render(f"Level:{level}",1,(255,255,255))
 
+        # Game labels
         WINDOW.blit(lives_label,(10,10))
         WINDOW.blit(level_label,(WIDTH - level_label.get_width() - 10,10))
+
+        ship.draw(WINDOW)
 
         pygame.display.update()
 
@@ -63,5 +84,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        keys = pygame.key.get_pressed()
 
 main()
